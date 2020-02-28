0285cbac5cc1b573252af8b7c2a2c6d01c9c1d56
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2020-02-28 22:56:37 +0900

    First

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+
+requirements = {
+    "install": ["unidecode", "inflect"],
+    "setup": [],
+    "test": [],
+    "doc": []
+}
+install_requires = requirements["install"]
+setup_requires = requirements["setup"]
+tests_require = requirements["test"]
+extras_require = {
+    k: v for k, v in requirements.items() if k not in ["install", "setup"]
+}
+
+dirname = os.path.dirname(__file__)
+setup(
+    name="tacotron_cleaner",
+    version="0.0.0",
+    url="http://github.com/espnet/tacotron_cleaner",
+    description="derived from https://github.com/keithito/tacotron",
+    packages=find_packages(include=["tacotron_cleaner*"]),
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    extras_require=extras_require,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
