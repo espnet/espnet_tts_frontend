62fd6e55e7ffca37c302ac351ebbcaa95af547fc
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2020-05-02 18:04:51 +0900

    add pythonpublish.yml

--- setup.py
+++ setup.py
@@ -74,7 +74,10 @@ extras_require = {
 
 setup(
     name="espnet_tts_frontend",
-    version="0.0.0",
+    version="0.0.1",
+    long_description=open(os.path.join(dirname, 'README.md'),
+                          encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
     url="http://github.com/espnet/espnet_tts_frontend",
     packages=find_packages(include=["tacotron_cleaner*", "vietnamese_cleaner*"]),
     install_requires=install_requires,
@@ -87,6 +90,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Science/Research",
         "Operating System :: POSIX :: Linux",
